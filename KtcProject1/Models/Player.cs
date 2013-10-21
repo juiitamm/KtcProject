3c5e9a5a46b6458567e9709ac50a994793a6dd21
juiitamm <juho.tammela@gmail.com>
2013-10-21 21:46:41 +0300

    Added error message for required field name.
    Added static method to get the message.

--- KtcProject1/Models/Player.cs
+++ KtcProject1/Models/Player.cs
@@ -9,9 +9,12 @@ namespace KtcProject1.Models
 {
     public class Player
     {
+        private const string NameRequiredError = "A name is required!";
+
         [DisplayName("Player Name")]
+        [Required(ErrorMessage = NameRequiredError)]
         public string Name { get; set; }
-        
+
         [ScaffoldColumn(false)]
         public int Games { get; set; }
         
@@ -23,5 +26,11 @@ namespace KtcProject1.Models
         
         [ScaffoldColumn(false)]
         public int Chimneys { get; set; }
+
+
+        public static string GetNameErrorMessage()
+        {
+            return NameRequiredError;
+        }
     }
 }
